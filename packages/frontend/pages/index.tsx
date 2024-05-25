64ad6adf89e131849cce7504dc815b0448bf40e7
0xErdem <134340437+0xErdem@users.noreply.github.com>
2024-05-26 00:46:33 +0300

    feat: frontend main page

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,118 +1,102 @@
-import Image from "next/image";
+"use client";
 import { Inter } from "next/font/google";
+import { generateSecretAndNullifier } from "@/utils";
+import React, { useState, useRef, useEffect } from "react";
+import { fetchSigned } from "@/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
+async function getUTXO(address: string, poseidonHash: string) {
+  let url = new URL("/api/getUTXO", "http://localhost:3001");
+  url.search = new URLSearchParams({ address, poseidonHash }).toString();
+
+  const result = await fetch(url.toString(), {
+    method: "GET",
+  }).then((res) => res.json());
+  console.log("RESULT:", result);
+  return result;
+}
+
 export default function Home() {
+  const [connected, setConnected] = useState(false);
+  const [accounts, setAccounts] = useState<string[]>([]);
+  const [bitaddress, setAddress] = useState("");
+  const isInitialMount = useRef(true);
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+    } else {
+      if (accounts.length > 0) {
+        setConnected(true);
+        localStorage.setItem("connected", "true");
+        setAddress(accounts[0]);
+        localStorage.setItem("bitAddress", accounts[0]);
+      } else {
+        setConnected(false);
+      }
+    }
+  }, [accounts]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const unisat = (window as any).unisat;
+      const checkUnisat = async () => {
+        try {
+          const accounts = await unisat.getAccounts();
+          setAccounts(accounts);
+        } catch (error) {
+          console.error("Error fetching Unisat accounts:", error);
+        }
+      };
+      checkUnisat();
+    }
+  }, []);
+
+  function truncateAddress(address: any) {
+    return address.slice(0, 6) + "..." + address.slice(-4);
+  }
+
+  const [commitment, setCommitment] = useState<string | null>(null);
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold">Welcome to your Next.js app!</h1>
+        <p className="text-lg mt-4">Get started by editing </p>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => {
+          generateSecretAndNullifier().then((commitment) => {
+            setCommitment(commitment);
+            console.log("Commitment:", commitment);
+            console.log("Nullifier:", localStorage.getItem("nullifier"));
+            console.log("Secret:", localStorage.getItem("secret"));
+          });
+        }}
+      >
+        Button
+      </button>
+      <button
+        className="bg-[#99DDE2] text-black font-sans font-bold border-black border-[3px]
+  border-solid text-center rounded-lg textcolor-white w-[188px] h-[40px] hover:bg-white border-l-8 border-b-8 mt-1 shadow-[100px_35px_35px_-15px_rgba(0,0,0,0)] flex items-center justify-center"
+        onClick={async () => {
+          const result = await unisat.requestAccounts();
+          console.log("Result unisat: ", result);
+        }}
+      >
+        Connect Unisat Wallet
+      </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => {
+          fetchSigned(unisat, bitaddress, commitment || "0x").then((result) => {
+            console.log("Result:", result);
+          });
+        }}
+      ></button>
+      {commitment && <p>Commitment: {commitment}</p>}
     </main>
   );
 }
